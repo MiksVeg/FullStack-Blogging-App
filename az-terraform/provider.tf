43701fc7e5bea0e3807382b2512adae94717212a
Soumik Mondal <119285411+MiksVeg@users.noreply.github.com>
2024-09-16 23:04:25 +0530

    Create provider.tf

---
+++ az-terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    subscription_id = "3175d3fd-c9f2-456c-b29f-c5dd2acaa632"
+  features {
+    
+  }
+}
